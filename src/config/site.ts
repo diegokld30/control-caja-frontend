034727c3cb046c99e5fc4be1af2ca739f0125e21
diegokld30 <dfercalderon@poligran.edu.co>
2025-03-22 10:20:21 -0500

    Configurando inicialmente los links de acceso de los clientes

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Vite + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Control de caja",
+  description: "Sistema de control de caja diaria",
   navItems: [
     {
       label: "Home",
@@ -60,7 +60,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/frontio-ai/heroui",
+    github: "https://github.com/diegokld30",
     twitter: "https://twitter.com/hero_ui",
     docs: "https://heroui.com",
     discord: "https://discord.gg/9b6yyZKmH4",
--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -17,11 +17,11 @@ export default function DefaultLayout({
         <Link
           isExternal
           className="flex items-center gap-1 text-current"
-          href="https://heroui.com"
-          title="heroui.com homepage"
+          href="https://www.facebook.com/SENAHuila/videos/territoriosena-en-el-sena-pitalito-sede-yamboró-pusimos-en-funcionamiento-la-est/297924495592704/"
+          title="superetehomepage"
         >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">HeroUI</p>
+          <span className="text-default-600">Creado para</span>
+          <p className="text-primary">Superete</p>
         </Link>
       </footer>
     </div>
